520b4aa9404de5ba344236c1221ac29cf8520d11
nampungggg <npueng04@gmail.com>
2024-11-11 05:22:56 +0000

    u6687019

--- test_zoo.py
+++ test_zoo.py
@@ -7,8 +7,36 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+        
+    
     # Add your additional test cases here.
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-5), "invalid")
+    def test_child_ticket_price1(self):
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+    def test_child_ticket_price2(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+    def test_child_ticket_price3(self):
+        self.assertEqual(self.zoo.get_ticket_price(22), 150)
+    def test_child_ticket_price4(self):
+        self.assertEqual(self.zoo.get_ticket_price(66), 100)
 
+    def test_child_ticket_price1(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "invalid")
+    def test_child_ticket_price2(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    def test_child_ticket_price3(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+    def test_child_ticket_price4(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+    def test_child_ticket_price1(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+    def test_child_ticket_price2(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+    def test_child_ticket_price3(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    def test_child_ticket_price4(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
 if __name__ == '__main__':
     unittest.main()
+    test=TestZoo()
--- zoo.py
+++ zoo.py
@@ -1,12 +1,12 @@
 class Zoo:
-    def get_ticket_price(self, age):
-        if 0 < age <= 12:
+ def get_ticket_price(self,age):
+        if age <13 and age > 0 or age==0:
             return 50
-        elif 13 <= age < 20:
+        elif  age < 21:
             return 100
-        elif 21 < age <= 60:
+        elif  age < 61:
             return 150
-        elif age >= 60:
+        elif age > 60:
             return 100
         else:
             return "invalid"
